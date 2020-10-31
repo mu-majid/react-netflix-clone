d9c9374a3134497c7e3edb452153def19deb3af9
Muhammad Majid <mmajidkamel@gmail.com>
2020-11-01 00:17:54 +0200

    Added styled component for the footer component

---
+++ src/components/footer/styles/footer.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+  display: flex;
+  padding: 70px 56px;
+  margin: auto;
+  max-width: 1000px;
+  flex-direction: column;
+
+  @media (max-width: 1000px) {
+    padding: 70px 30px;
+  }
+`;
+
+export const Title = styled.div``;
+export const Text = styled.div``;
+
+export const Row = styled.div``;
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+`;
+export const Link = styled.a``;
+export const Break = styled.a``;
+
