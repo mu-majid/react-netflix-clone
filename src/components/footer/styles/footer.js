6360cb3a1df4c68f0d9e05cf75374f117f4d21cc
Muhammad Majid <mmajidkamel@gmail.com>
2020-11-03 00:31:59 +0200

    footer styling done

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -12,16 +12,42 @@ export const Container = styled.div`
   }
 `;
 
-export const Title = styled.div``;
-export const Text = styled.div``;
-
-export const Row = styled.div``;
 export const Column = styled.div`
   display: flex;
   flex-direction: column;
   text-align: left;
 `;
-export const Link = styled.a``;
-export const Break = styled.a``;
 
+export const Row = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+  grid-gap: 15px;
 
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  }
+`;
+
+export const Link = styled.a`
+  color: #757575;
+  margin-bottom: 20px;
+  font-size: 13px;
+  text-decoration: none;
+`;
+
+export const Title = styled.p`
+  font-size: 16px;
+  color: #757575;
+  margin-bottom: 40px;
+`;
+
+export const Text = styled.p`
+  font-size: 13px;
+  color: #757575;
+  margin-bottom: 40px;
+`;
+
+export const Break = styled.div`
+  flex-basis: 100%;
+  height: 0;
+`;
