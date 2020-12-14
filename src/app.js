d3ce244ebdf67e2acef8a2624319f2b091f7d94c
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-14 02:27:32 +0200

    setting up react routing

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { JumbotronContainer } from './containers/jumbotron';
-import { FooterContainer } from './containers/footer';
-import { FaqsContainer } from './containers/faqs';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home, Browse, Signin, Signup } from './pages';
+import * as ROUTES from './constants/routes';
 
-
+// this component should show what out app is doing
+// so it should container react router here instead of 
+// home page or any page related component
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+
+      <Route exact path={ROUTES.BROWSE}>
+        <Browse />
+      </Route>
+
+      <Route exact path={ROUTES.SIGN_IN}>
+        <Signin />
+      </Route>
+
+      <Route exact path={ROUTES.SIGN_UP}>
+        <Signup />
+      </Route>
+      
+      <Route exact path={ROUTES.HOME}>
+        <Home />
+      </Route>
+
+    </Router>
   );
 }
